22096dcf7af39105f5a4d5aecd5a3508f099910a
nlisnyak <lisnyaknikita@gmail.com>
2025-09-08 16:11:01 +0300

    implemented modal window for event creation

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,18 +1,21 @@
 'use client'
 
+import { Modal } from '@/shared/ui/modal/modal'
 import { useEffect, useState } from 'react'
 import { BeatLoader } from 'react-spinners'
+import { EventModal } from './components/event-modal/event-modal'
 import { CreateButton } from './components/header/create-button/create-button'
 import { Tabs } from './components/header/tabs/tabs'
 import { CalendarInner } from './components/main/calendar/calendar'
 import classes from './page.module.scss'
 
-export type CalendarView = 'month' | 'week' | 'day' | null
+export type CalendarView = 'month' | 'week' | 'day'
 
 const VIEW_KEY = 'calendarView'
 
 export default function Calendar() {
 	const [view, setView] = useState<CalendarView | null>(null)
+	const [isModalVisible, setIsModalVisible] = useState(false)
 
 	useEffect(() => {
 		const saved = localStorage.getItem(VIEW_KEY) as CalendarView | null
@@ -24,14 +27,25 @@ export default function Calendar() {
 	}, [view])
 
 	return (
-		<div className={classes.calendarPage}>
-			<header className={classes.header}>
-				<Tabs activeTab={view} onChange={setView} />
-				<CreateButton />
-			</header>
-			<main className={classes.calendar}>
-				{!view ? <BeatLoader color='#aaa' size={10} className={classes.loader} /> : <CalendarInner view={view} />}
-			</main>
-		</div>
+		<>
+			<div className={classes.calendarPage}>
+				{!view ? (
+					<BeatLoader color='#aaa' size={10} className={classes.loader} />
+				) : (
+					<>
+						<header className={classes.header}>
+							<Tabs activeTab={view} onChange={setView} />
+							<CreateButton setIsModalVisible={setIsModalVisible} />
+						</header>
+						<main className={classes.calendar}>
+							<CalendarInner view={view} />
+						</main>
+					</>
+				)}
+			</div>
+			<Modal isVisible={isModalVisible} onClose={() => setIsModalVisible(false)}>
+				<EventModal onClose={() => setIsModalVisible(false)} />
+			</Modal>
+		</>
 	)
 }
