ef4b6858fab6878beb03b2ed2692c4871287568a
nlisnyak <lisnyaknikita@gmail.com>
2025-09-10 19:27:05 +0300

    fix label hover

--- src/app/focus/header/timer-settings-modal/timer-settings-modal.tsx
+++ src/app/focus/header/timer-settings-modal/timer-settings-modal.tsx
@@ -7,18 +7,18 @@ export const TimerSettingsModal = () => {
 			<h3 className={classes.title}>Pomodoro settings</h3>
 			<h6 className={classes.subtitle}>Customize your focus flow</h6>
 			<form className={classes.settingsForm}>
-				<label className={classes.formItem}>
+				<div className={classes.formItem}>
 					<span className={classes.label}>Flow duration</span>
 					<DurationPicker type='flow' />
-				</label>
-				<label className={classes.formItem}>
+				</div>
+				<div className={classes.formItem}>
 					<span className={classes.label}>Break duration</span>
 					<DurationPicker type='break' />
-				</label>
-				<label className={classes.formItem}>
+				</div>
+				<div className={classes.formItem}>
 					<span className={classes.label}>Number of sessions</span>
 					<DurationPicker type='sessions' />
-				</label>
+				</div>
 			</form>
 		</div>
 	)
