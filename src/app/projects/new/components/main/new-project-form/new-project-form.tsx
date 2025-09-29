4fe2129f1c20358e5e3b0f80159b4a925197152e
nlisnyak <lisnyaknikita@gmail.com>
2025-09-29 18:21:31 +0300

    implemented window with team invites and corrected light theme styles

--- src/app/projects/new/components/main/new-project-form/new-project-form.tsx
+++ src/app/projects/new/components/main/new-project-form/new-project-form.tsx
@@ -1,18 +1,39 @@
 import { useState } from 'react'
 
 import { RadioCard } from '@/shared/ui/radio-card/radio-card'
+import { useRouter } from 'next/navigation'
 import classes from './new-project-form.module.scss'
 
 export const NewProjectForm = () => {
+	const [title, setTitle] = useState('')
 	const [projectType, setProjectType] = useState<'solo' | 'team' | null>(null)
+	const router = useRouter()
+
+	const handleCreation = (e: React.FormEvent) => {
+		e.preventDefault()
+		if (!title || !projectType) return
+
+		const projectId = 123
+
+		if (projectType === 'team') {
+			router.push(`/projects/new/team?projectId=${projectId}`)
+		} else {
+			router.push(`/projects/${projectId}`)
+		}
+	}
 
 	return (
-		<form className={classes.newProjectForm}>
+		<form className={classes.newProjectForm} onSubmit={handleCreation}>
 			<h3 className={classes.formTitle}>Create new project</h3>
 			<p className={classes.formSubtitle}>Start organizing your work efficiently</p>
 			<div className={classes.titleLabel}>
 				<span>Project title</span>
-				<input type='text' placeholder='Enter your project name' />
+				<input
+					type='text'
+					placeholder='Enter your project name'
+					value={title}
+					onChange={e => setTitle(e.target.value)}
+				/>
 			</div>
 			<div className={classes.projectTypeLabel}>
 				<span>Project type</span>
