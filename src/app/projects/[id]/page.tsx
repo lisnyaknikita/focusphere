dbe8d9995975cf64dfba840b45fa37e8b4a9e942
nlisnyak <lisnyaknikita@gmail.com>
2025-10-10 18:42:59 +0300

    fixed await params in page.tsx

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from 'next/navigation'
 
-export default function ProjectPage({ params }: { params: { id: string } }) {
-	redirect(`/projects/${params.id}/board`)
+export default async function ProjectPage({ params }: { params: Promise<{ id: string }> }) {
+	const { id } = await params
+
+	redirect(`/projects/${id}/board`)
 }
