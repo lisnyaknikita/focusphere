e1a8f94dd9727e1e630a1af815cb14e711ec95dd
nlisnyak <lisnyaknikita@gmail.com>
2025-10-10 17:55:02 +0300

    reuse notes list, text editor components and implemented tabs components on notes page

--- src/app/projects/[id]/notes/page.tsx
+++ src/app/projects/[id]/notes/page.tsx
@@ -1,5 +1,16 @@
+import { NotesList } from '@/shared/ui/notes-list/notes-list'
+import { TextEditor } from '@/shared/ui/text-editor/text-editor'
+import { Tags } from './components/tags/tags'
 import classes from './page.module.scss'
 
 export default function NotesPage() {
-	return <div className={classes.boardPage}>notes page</div>
+	return (
+		<div className={classes.notesPage}>
+			<div className={classes.inner}>
+				<NotesList withTitle={true} withTags={true} />
+				<TextEditor />
+				<Tags />
+			</div>
+		</div>
+	)
 }
--- src/shared/ui/notes-list/notes-list.tsx
+++ src/shared/ui/notes-list/notes-list.tsx
@@ -1,143 +1,152 @@
+import clsx from 'clsx'
 import classes from './notes-list.module.scss'
 
-export const NotesList = () => {
+interface NotesListProps {
+	withTitle: boolean
+	withTags: boolean
+}
+
+export const NotesList = ({ withTitle, withTags }: NotesListProps) => {
 	return (
-		<ul className={classes.notesList}>
-			<li className={classes.notesItem}>
-				<a href='#' className={classes.notesItemLink}>
-					<div className={classes.date}>
-						<span className={classes.day}>Wed</span>
-						<span className={classes.number}>03</span>
-					</div>
-					<div className={classes.content}>
-						<h4 className={classes.noteTitle}>Project team</h4>
-						<h6 className={classes.noteSubtitle}>
-							I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-						</h6>
-						<time className={classes.noteTime}>09:31 AM</time>
-					</div>
-				</a>
-			</li>
-			<li className={classes.notesItem}>
-				<a href='#' className={classes.notesItemLink}>
-					<div className={classes.date}>
-						<span className={classes.day}>Wed</span>
-						<span className={classes.number}>03</span>
-					</div>
-					<div className={classes.content}>
-						<h4 className={classes.noteTitle}>Project team</h4>
-						<h6 className={classes.noteSubtitle}>
-							I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-						</h6>
-						<time className={classes.noteTime}>09:31 AM</time>
-					</div>
-				</a>
-			</li>
-			<li className={classes.notesItem}>
-				<a href='#' className={classes.notesItemLink}>
-					<div className={classes.date}>
-						<span className={classes.day}>Wed</span>
-						<span className={classes.number}>03</span>
-					</div>
-					<div className={classes.content}>
-						<h4 className={classes.noteTitle}>Project team</h4>
-						<h6 className={classes.noteSubtitle}>
-							I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-						</h6>
-						<time className={classes.noteTime}>09:31 AM</time>
-					</div>
-				</a>
-			</li>
-			<li className={classes.notesItem}>
-				<a href='#' className={classes.notesItemLink}>
-					<div className={classes.date}>
-						<span className={classes.day}>Wed</span>
-						<span className={classes.number}>03</span>
-					</div>
-					<div className={classes.content}>
-						<h4 className={classes.noteTitle}>Project team</h4>
-						<h6 className={classes.noteSubtitle}>
-							I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-						</h6>
-						<time className={classes.noteTime}>09:31 AM</time>
-					</div>
-				</a>
-			</li>
-			<li className={classes.notesItem}>
-				<a href='#' className={classes.notesItemLink}>
-					<div className={classes.date}>
-						<span className={classes.day}>Wed</span>
-						<span className={classes.number}>03</span>
-					</div>
-					<div className={classes.content}>
-						<h4 className={classes.noteTitle}>Project team</h4>
-						<h6 className={classes.noteSubtitle}>
-							I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-						</h6>
-						<time className={classes.noteTime}>09:31 AM</time>
-					</div>
-				</a>
-			</li>
-			<li className={classes.notesItem}>
-				<a href='#' className={classes.notesItemLink}>
-					<div className={classes.date}>
-						<span className={classes.day}>Wed</span>
-						<span className={classes.number}>03</span>
-					</div>
-					<div className={classes.content}>
-						<h4 className={classes.noteTitle}>Project team</h4>
-						<h6 className={classes.noteSubtitle}>
-							I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-						</h6>
-						<time className={classes.noteTime}>09:31 AM</time>
-					</div>
-				</a>
-			</li>
-			<li className={classes.notesItem}>
-				<a href='#' className={classes.notesItemLink}>
-					<div className={classes.date}>
-						<span className={classes.day}>Wed</span>
-						<span className={classes.number}>03</span>
-					</div>
-					<div className={classes.content}>
-						<h4 className={classes.noteTitle}>Project team</h4>
-						<h6 className={classes.noteSubtitle}>
-							I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-						</h6>
-						<time className={classes.noteTime}>09:31 AM</time>
-					</div>
-				</a>
-			</li>
-			<li className={classes.notesItem}>
-				<a href='#' className={classes.notesItemLink}>
-					<div className={classes.date}>
-						<span className={classes.day}>Wed</span>
-						<span className={classes.number}>03</span>
-					</div>
-					<div className={classes.content}>
-						<h4 className={classes.noteTitle}>Project team</h4>
-						<h6 className={classes.noteSubtitle}>
-							I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-						</h6>
-						<time className={classes.noteTime}>09:31 AM</time>
-					</div>
-				</a>
-			</li>
-			<li className={classes.notesItem}>
-				<a href='#' className={classes.notesItemLink}>
-					<div className={classes.date}>
-						<span className={classes.day}>Wed</span>
-						<span className={classes.number}>03</span>
-					</div>
-					<div className={classes.content}>
-						<h4 className={classes.noteTitle}>Project team</h4>
-						<h6 className={classes.noteSubtitle}>
-							I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-						</h6>
-						<time className={classes.noteTime}>09:31 AM</time>
-					</div>
-				</a>
-			</li>
-		</ul>
+		<div className={clsx(classes.listWrapper, withTags && 'withTags')}>
+			{withTitle && <h3 className={classes.title}>Website redesign</h3>}
+			<ul className={classes.notesList}>
+				<li className={classes.notesItem}>
+					<a href='#' className={classes.notesItemLink}>
+						<div className={classes.date}>
+							<span className={classes.day}>Wed</span>
+							<span className={classes.number}>03</span>
+						</div>
+						<div className={classes.content}>
+							<h4 className={classes.noteTitle}>Project team</h4>
+							<h6 className={classes.noteSubtitle}>
+								I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+							</h6>
+							<time className={classes.noteTime}>09:31 AM</time>
+						</div>
+					</a>
+				</li>
+				<li className={classes.notesItem}>
+					<a href='#' className={classes.notesItemLink}>
+						<div className={classes.date}>
+							<span className={classes.day}>Wed</span>
+							<span className={classes.number}>03</span>
+						</div>
+						<div className={classes.content}>
+							<h4 className={classes.noteTitle}>Project team</h4>
+							<h6 className={classes.noteSubtitle}>
+								I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+							</h6>
+							<time className={classes.noteTime}>09:31 AM</time>
+						</div>
+					</a>
+				</li>
+				<li className={classes.notesItem}>
+					<a href='#' className={classes.notesItemLink}>
+						<div className={classes.date}>
+							<span className={classes.day}>Wed</span>
+							<span className={classes.number}>03</span>
+						</div>
+						<div className={classes.content}>
+							<h4 className={classes.noteTitle}>Project team</h4>
+							<h6 className={classes.noteSubtitle}>
+								I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+							</h6>
+							<time className={classes.noteTime}>09:31 AM</time>
+						</div>
+					</a>
+				</li>
+				<li className={classes.notesItem}>
+					<a href='#' className={classes.notesItemLink}>
+						<div className={classes.date}>
+							<span className={classes.day}>Wed</span>
+							<span className={classes.number}>03</span>
+						</div>
+						<div className={classes.content}>
+							<h4 className={classes.noteTitle}>Project team</h4>
+							<h6 className={classes.noteSubtitle}>
+								I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+							</h6>
+							<time className={classes.noteTime}>09:31 AM</time>
+						</div>
+					</a>
+				</li>
+				<li className={classes.notesItem}>
+					<a href='#' className={classes.notesItemLink}>
+						<div className={classes.date}>
+							<span className={classes.day}>Wed</span>
+							<span className={classes.number}>03</span>
+						</div>
+						<div className={classes.content}>
+							<h4 className={classes.noteTitle}>Project team</h4>
+							<h6 className={classes.noteSubtitle}>
+								I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+							</h6>
+							<time className={classes.noteTime}>09:31 AM</time>
+						</div>
+					</a>
+				</li>
+				<li className={classes.notesItem}>
+					<a href='#' className={classes.notesItemLink}>
+						<div className={classes.date}>
+							<span className={classes.day}>Wed</span>
+							<span className={classes.number}>03</span>
+						</div>
+						<div className={classes.content}>
+							<h4 className={classes.noteTitle}>Project team</h4>
+							<h6 className={classes.noteSubtitle}>
+								I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+							</h6>
+							<time className={classes.noteTime}>09:31 AM</time>
+						</div>
+					</a>
+				</li>
+				<li className={classes.notesItem}>
+					<a href='#' className={classes.notesItemLink}>
+						<div className={classes.date}>
+							<span className={classes.day}>Wed</span>
+							<span className={classes.number}>03</span>
+						</div>
+						<div className={classes.content}>
+							<h4 className={classes.noteTitle}>Project team</h4>
+							<h6 className={classes.noteSubtitle}>
+								I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+							</h6>
+							<time className={classes.noteTime}>09:31 AM</time>
+						</div>
+					</a>
+				</li>
+				<li className={classes.notesItem}>
+					<a href='#' className={classes.notesItemLink}>
+						<div className={classes.date}>
+							<span className={classes.day}>Wed</span>
+							<span className={classes.number}>03</span>
+						</div>
+						<div className={classes.content}>
+							<h4 className={classes.noteTitle}>Project team</h4>
+							<h6 className={classes.noteSubtitle}>
+								I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+							</h6>
+							<time className={classes.noteTime}>09:31 AM</time>
+						</div>
+					</a>
+				</li>
+				<li className={classes.notesItem}>
+					<a href='#' className={classes.notesItemLink}>
+						<div className={classes.date}>
+							<span className={classes.day}>Wed</span>
+							<span className={classes.number}>03</span>
+						</div>
+						<div className={classes.content}>
+							<h4 className={classes.noteTitle}>Project team</h4>
+							<h6 className={classes.noteSubtitle}>
+								I am so tired is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+							</h6>
+							<time className={classes.noteTime}>09:31 AM</time>
+						</div>
+					</a>
+				</li>
+			</ul>
+		</div>
 	)
 }
