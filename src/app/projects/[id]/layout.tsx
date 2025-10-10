d4f255e24d44c12b9bc4a63da2121d2099cc62ad
nlisnyak <lisnyaknikita@gmail.com>
2025-10-10 18:37:52 +0300

    fixed await params

--- src/app/projects/[id]/layout.tsx
+++ src/app/projects/[id]/layout.tsx
@@ -1,11 +1,19 @@
 import { ProjectTabs } from './components/tabs/tabs'
 import classes from './page.module.scss'
 
-export default function ProjectLayout({ children, params }: { children: React.ReactNode; params: { id: string } }) {
+export default async function ProjectLayout({
+	children,
+	params,
+}: {
+	children: React.ReactNode
+	params: Promise<{ id: string }>
+}) {
+	const { id } = await params
+
 	return (
 		<div className={classes.projectPage}>
 			<header className={classes.header}>
-				<ProjectTabs projectId={params.id} />
+				<ProjectTabs projectId={id} />
 			</header>
 			<main className={classes.main}>{children}</main>
 		</div>
