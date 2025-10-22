10d366c31000fc0c84a6db80b1332c8991e1c22a
nlisnyak <lisnyaknikita@gmail.com>
2025-10-22 22:57:02 +0300

    implemented hook for dynamic list max-height calculation

--- front/src/app/(dashboard)/components/dashboard-block/components/tasks/tasks.tsx
+++ front/src/app/(dashboard)/components/dashboard-block/components/tasks/tasks.tsx
@@ -2,13 +2,16 @@
 
 import { CheckboxCard } from '@/shared/ui/checkbox-card/checkbox-card'
 
+import { useSectionHeight } from '@/shared/hooks/section-height/useSectionHeight'
 import classes from './tasks.module.scss'
 
 export const TasksBlock = () => {
+	const { sectionRef, listHeight } = useSectionHeight(0.73)
+
 	return (
-		<section className={classes.tasks}>
+		<section className={classes.tasks} ref={sectionRef}>
 			<h2>Tasks for today:</h2>
-			<ul className={classes.tasksList}>
+			<ul className={classes.tasksList} style={{ maxHeight: `${listHeight}px` }}>
 				<li>
 					<CheckboxCard label='Task 1' checked={false} onCheck={() => {}} withBorder={true} />
 				</li>
