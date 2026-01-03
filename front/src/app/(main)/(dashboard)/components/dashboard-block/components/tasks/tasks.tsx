d921d19e6696d5c5b2129e2193dbe94df7ad5c13
nlisnyak <lisnyaknikita@gmail.com>
2026-01-03 12:26:07 +0200

    displayed daily tasks for today

--- front/src/app/(main)/(dashboard)/components/dashboard-block/components/tasks/tasks.tsx
+++ front/src/app/(main)/(dashboard)/components/dashboard-block/components/tasks/tasks.tsx
@@ -2,32 +2,36 @@
 
 import { CheckboxCard } from '@/shared/ui/checkbox-card/checkbox-card'
 
+import { useTasksByToday } from '@/shared/hooks/daily-tasks/use-tasks-by-today'
 import { useSectionHeight } from '@/shared/hooks/section-height/useSectionHeight'
+import { BeatLoader } from 'react-spinners'
 import classes from './tasks.module.scss'
 
 export const TasksBlock = () => {
 	const { sectionRef, listHeight } = useSectionHeight(0.73)
+	const { tasks, isLoading, handleToggleTask } = useTasksByToday()
 
 	return (
 		<section className={classes.tasks} ref={sectionRef}>
 			<h2>Tasks for today:</h2>
-			<ul className={classes.tasksList} style={{ maxHeight: `${listHeight}px` }}>
-				<li>
-					<CheckboxCard label='Task 1' checked={false} onCheck={() => {}} withBorder={true} />
-				</li>
-				<li>
-					<CheckboxCard label='Task 2' checked={true} onCheck={() => {}} withBorder={true} />
-				</li>
-				<li>
-					<CheckboxCard label='Task 3' checked={true} onCheck={() => {}} withBorder={true} />
-				</li>
-				<li>
-					<CheckboxCard label='Task 4' checked={true} onCheck={() => {}} withBorder={true} />
-				</li>
-				<li>
-					<CheckboxCard label='Task 5' checked={true} onCheck={() => {}} withBorder={true} />
-				</li>
-			</ul>
+			{tasks.length === 0 && !isLoading ? (
+				<p className={classes.noTasksMessage}>No tasks for today</p>
+			) : tasks.length === 0 ? (
+				<BeatLoader color='#aaa' size={10} className={classes.loader} />
+			) : (
+				<ul className={classes.tasksList} style={{ maxHeight: `${listHeight}px` }}>
+					{tasks.map(item => (
+						<li key={item.$id}>
+							<CheckboxCard
+								label={item.title}
+								checked={item.isCompleted}
+								onCheck={checked => handleToggleTask(item.$id, checked)}
+								withBorder={true}
+							/>
+						</li>
+					))}
+				</ul>
+			)}
 		</section>
 	)
 }
