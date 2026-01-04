1b0306326a3dda2c9ecc5b5a7f345de64db6e77f
nlisnyak <lisnyaknikita@gmail.com>
2026-01-04 13:35:59 +0200

    fix reset-password useSearchParams

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,23 +1,31 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 import classes from './page.module.scss'
 import { ResetPasswordForm } from './reset-password-form/reset-password-form'
 
-export default function ResetPasswordPage() {
+function ResetPasswordContent() {
 	const params = useSearchParams()
-
 	const userId = params.get('userId')
 	const secret = params.get('secret')
 
 	if (!userId || !secret) return <p>Invalid password reset link.</p>
 
 	return (
+		<div className={classes.createPasswordBlock}>
+			<h1 className={classes.title}>Create a new password</h1>
+			<ResetPasswordForm userId={userId} secret={secret} />
+		</div>
+	)
+}
+
+export default function ResetPasswordPage() {
+	return (
 		<div className={classes.createPasswordPage}>
-			<div className={classes.createPasswordBlock}>
-				<h1 className={classes.title}>Create a new password</h1>
-				<ResetPasswordForm userId={userId} secret={secret} />
-			</div>
+			<Suspense fallback={<p>Loading...</p>}>
+				<ResetPasswordContent />
+			</Suspense>
 		</div>
 	)
 }
