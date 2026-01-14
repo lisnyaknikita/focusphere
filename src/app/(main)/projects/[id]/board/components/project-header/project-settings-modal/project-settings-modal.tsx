4df4012b6bfcf8829d233c3014f180daa73166ba
nlisnyak <lisnyaknikita@gmail.com>
2026-01-14 17:15:02 +0200

    implemented project deletion with its own tasks

--- src/app/(main)/projects/[id]/board/components/project-header/project-settings-modal/project-settings-modal.tsx
+++ src/app/(main)/projects/[id]/board/components/project-header/project-settings-modal/project-settings-modal.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import { useDeleteProject } from '@/shared/hooks/projects/use-delete-project'
 import { useUpdateProject } from '@/shared/hooks/projects/use-update-project'
 import { Project } from '@/shared/types/project'
 import { RadioCard } from '@/shared/ui/radio-card/radio-card'
+import { useRouter } from 'next/navigation'
 import { Controller } from 'react-hook-form'
 import classes from './project-settings-modal.module.scss'
 
@@ -13,18 +15,33 @@ interface ProjectSettingsModalProps {
 
 export const ProjectSettingsModal = ({ project, onClose }: ProjectSettingsModalProps) => {
 	const { register, control, onSubmit, isSubmitting, errors } = useUpdateProject({ project, onSuccess: onClose })
+	const { remove, isDeleting } = useDeleteProject()
+
+	const router = useRouter()
+
+	const handleDelete = async () => {
+		await remove(project.$id, () => {
+			onClose()
+			router.push('/projects')
+			router.refresh()
+		})
+	}
 
 	return (
 		<form className={classes.settingsForm} onSubmit={onSubmit}>
 			<h3 className={classes.title}>Project Settings</h3>
 			<label className={classes.field}>
 				<span>Project title</span>
-				<input type='text' {...register('title')} disabled={isSubmitting} />
+				<input type='text' {...register('title')} disabled={isSubmitting || isDeleting} />
 				{errors.title && <p className={classes.errorText}>{errors.title.message}</p>}
 			</label>
 			<label className={classes.field}>
 				<span>Description</span>
-				<textarea {...register('description')} placeholder='Add a short description...' disabled={isSubmitting} />
+				<textarea
+					{...register('description')}
+					placeholder='Add a short description...'
+					disabled={isSubmitting || isDeleting}
+				/>
 			</label>
 			<div className={classes.radioField}>
 				<label>Project type</label>
@@ -52,9 +69,19 @@ export const ProjectSettingsModal = ({ project, onClose }: ProjectSettingsModalP
 				/>
 				{errors.type && <p className={classes.errorText}>{errors.type.message}</p>}
 			</div>
-			<button className={classes.saveButton} disabled={isSubmitting}>
-				{isSubmitting ? 'Saving...' : 'Save Changes'}
-			</button>
+			<div className={classes.buttons}>
+				<button className={classes.saveButton} disabled={isSubmitting}>
+					{isSubmitting ? 'Saving...' : 'Save Changes'}
+				</button>
+				<button
+					type='button'
+					className={classes.deleteButton}
+					onClick={handleDelete}
+					disabled={isDeleting || isSubmitting}
+				>
+					{isDeleting ? 'Deleting...' : 'Delete project'}
+				</button>
+			</div>
 		</form>
 	)
 }
--- src/lib/projects/projects.ts
+++ src/lib/projects/projects.ts
@@ -1,5 +1,7 @@
+import { KanbanTask } from '@/shared/types/kanban-task'
 import { CreateProjectPayload, Project } from '@/shared/types/project'
 import { Client, ID, TablesDB } from 'appwrite'
+import { deleteKanbanTask, getKanbanTasks } from './kanban-board-tasks/tasks'
 
 const client = new Client()
 	.setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
@@ -32,6 +34,24 @@ export const updateProject = async (projectId: string, data: Partial<CreateProje
 		tableId: process.env.NEXT_PUBLIC_TABLE_PROJECTS!,
 		rowId: projectId,
 		data,
+	})
+
+	return response as unknown as Project
+}
+
+export const deleteProject = async (projectId: string): Promise<Project> => {
+	const tasksRes = await getKanbanTasks(projectId)
+
+	const tasks = tasksRes.rows as unknown as KanbanTask[]
+
+	const deleteTasksPromises = tasks.map(task => deleteKanbanTask(task.$id))
+
+	await Promise.all(deleteTasksPromises)
+
+	const response = await tablesDB.deleteRow({
+		databaseId: process.env.NEXT_PUBLIC_DB_ID!,
+		tableId: process.env.NEXT_PUBLIC_TABLE_PROJECTS!,
+		rowId: projectId,
 	})
 
 	return response as unknown as Project
