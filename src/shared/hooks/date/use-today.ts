51af1c0a813a47b7195a27084394b443057c872e
nlisnyak <lisnyaknikita@gmail.com>
2026-02-06 15:04:58 +0200

    fixed current date in dashboard page

--- src/shared/hooks/date/use-today.ts
+++ src/shared/hooks/date/use-today.ts
@@ -1,13 +1,30 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import 'temporal-polyfill/global'
 
-export const useToday = () => {
-	const today = Temporal.Now.plainDateISO()
+interface TodayData {
+	plainDate: Temporal.PlainDate
+	iso: string
+	day: number
+	weekday: string
+	month: string
+}
 
-	return {
-		plainDate: today,
-		iso: today.toString(),
-		day: today.day,
-		weekday: today.toLocaleString('en-US', { weekday: 'long' }),
-		month: today.toLocaleString('en-US', { month: 'long' }),
-	}
+export const useToday = () => {
+	const [todayData, setTodayData] = useState<TodayData | null>(null)
+
+	useEffect(() => {
+		const today = Temporal.Now.plainDateISO()
+
+		setTodayData({
+			plainDate: today,
+			iso: today.toString(),
+			day: today.day,
+			weekday: today.toLocaleString('en-US', { weekday: 'long' }),
+			month: today.toLocaleString('en-US', { month: 'long' }),
+		})
+	}, [])
+
+	return todayData
 }
