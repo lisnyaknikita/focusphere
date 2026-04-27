ac0380b133f2da67cb17483e79fbab3ae3fe18de
nlisnyak <lisnyaknikita@gmail.com>
2026-04-27 12:26:50 +0200

    changed app url for production

--- src/shared/constants/app.ts
+++ src/shared/constants/app.ts
@@ -1,2 +1,1 @@
-export const APP_URL =
-	process.env.NODE_ENV === 'production' ? 'https://focusphere-test.vercel.app' : 'http://localhost:3000'
+export const APP_URL = process.env.NODE_ENV === 'production' ? 'https://www.focusphere.org' : 'http://localhost:3000'
